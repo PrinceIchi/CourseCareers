73720668e09953555679884be765957e8cf05992
Prince <principe3219@icloud.com>
2024-12-08 12:54:55 -0800

    new day

--- test.js
+++ test.js
@@ -1,2 +1,29 @@
-console.log(document.getElementById("name").innerHTML)
+datatypes
 
+//string
+anything inside double or single quotation marks
+'76'
+"hello"
+"pete's"
+
+//number
+actual numbers
+123
+-2
+654
+0.5
+
+//boolean
+true or false statements
+
+//undefined
+somethin that is not defined or has no value
+
+//null
+something that does not exist
+
+//object
+anything inside curly brackets {}
+
+//NaN
+not a number
