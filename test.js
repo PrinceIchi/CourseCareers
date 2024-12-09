69b8413940f08e250fa39e7c23c761aecedee70c
Prince <principe3219@icloud.com>
2024-12-08 16:43:44 -0800

    more practice

--- test.js
+++ test.js
@@ -1,29 +1,5 @@
-datatypes
+const age = 18;
 
-//string
-anything inside double or single quotation marks
-'76'
-"hello"
-"pete's"
-
-//number
-actual numbers
-123
--2
-654
-0.5
-
-//boolean
-true or false statements
-
-//undefined
-somethin that is not defined or has no value
-
-//null
-something that does not exist
-
-//object
-anything inside curly brackets {}
-
-//NaN
-not a number
+if (age >= 16) {
+    console.log('you can drive');
+}
