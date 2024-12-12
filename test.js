57baad4c9cb9e6cfb999662b4d9f81c9b570d826
Prince <principe3219@icloud.com>
2024-12-11 20:17:29 -0800

    more practice

--- test.js
+++ test.js
@@ -1,17 +1,6 @@
-const matrix = [];
-
-for (let i = 0; i < 6; i++) {
-    const numbers = [];
-
-    
-        for (let j = i * 2; j < i * 2 + 2; j++) {
-            numbers.push(j + 1);
-            for (let k = 0; k < 6; k++) {
-                const for3 = [];
-        
-        }
-    }
-    matrix.push(numbers);
+while (true) {
+   const number = Number(prompt("enter a number between one and ten"));
+   if (number >= 1 && number <= 10) break;
 }
 
-console.log(matrix);
+alert(number);
