1abbb9123416184dd7c733627cea44ae94c37148
Prince <principe3219@icloud.com>
2024-12-14 20:39:47 -0800

    more practice

--- test.js
+++ test.js
@@ -1,23 +1,4 @@
-let arr = ['west side', '1987', 'i am the best', 'oh yeah']
-
-function removeAllChars(arrayOfStrings, charactersToRemove) {
-   const newArray = [];
-
-   for (const str of arrayOfStrings) {
-      let newStr = "";
-      for (const char of str) {
-         if (charactersToRemove.includes(char)) {
-            continue;
-         }
-         newStr += char;
-      }
-      newArray.push(newStr);
-   }
-   return newArray;
+function enterUsername() {
+   const username = document.getElementById("username").value;
+   document.getElementById("text").innerHTML = username;
 }
-
-const result = removeAllChars(
-   arr,
-   ['9', 's', 'l']
-);
-console.log(result)
